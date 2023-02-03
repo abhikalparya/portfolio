7a6a3d18b422d16303433f6b75f500a5ca040882
Abhikalp Arya <abhikalp.arya@gmail.com>
2023-02-03 18:00:00 +0530

    added sidebar

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// element toggle function
+const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
+
+// sidebar variables
+const sidebar = document.querySelector("[data-sidebar]");
+const sidebarBtn = document.querySelector("[data-sidebar-btn]");
+
+// sidebar toggle functionality for mobile
+sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); });
